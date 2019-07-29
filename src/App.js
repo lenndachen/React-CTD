162a5cb402f063f71645d9720830366c41927a7d
Lennda <37259500+chenl7460@users.noreply.github.com>
2019-07-29 09:18:11 -0400

    react-ctd work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
   return (
-    <div>{props.name}</div>
-  );
+    <div>
+      <h1>{props.intro} {props.name}
+      </h1>
+      <h3>Things learned at CTD classes</h3>
+      <h5>
+        <li>{props.item1}</li>
+        <li>{props.item2}</li>
+        <li>{props.item3}</li>
+        <li>{props.item4}</li>
+      </h5>
+    </div> 
+  )
 }
 
 export default App;
+
+
+/*{ category: "JS", subject: "Functions", proficiency: "7" }
+{ category: "JS", subject: "Methods", proficiency: "3" }
+{ category: "JS", subject: "Loops", proficiency: "6" }];*/
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App name='hello' />, document.getElementById('root'));
+ReactDOM.render(<App intro='hello' name='stranger'
+    item1="Objects" item2="Operators" item3="Functions" item4="Boolean"/>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
